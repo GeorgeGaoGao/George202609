39574b4a5ee3a4a566d64f03214496088bd551c8
GeorgeGao <georgeeasycfg@163.com>
2026-09-01 16:05:35 +0800

    20260901 afternoon

---
+++ GeorgeWpfDLL/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace GeorgeWpfDLL
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+        public RelayCommand(Action<object> execute,Func<object,bool> canExecute)
+        {
+            _execute = execute??throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+        public event EventHandler? CanExecuteChanged;
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute==null|| _canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
