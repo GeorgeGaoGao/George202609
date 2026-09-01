ad50188ebfe96fa12568a8d3e8db1109baefe380
GeorgeGao <georgeeasycfg@163.com>
2026-09-01 15:53:09 +0800

    00

---
+++ GeorgeWpfDLL/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeorgeWpfDLL
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age  { get; set; }
+        public string Address { get; set; }
+    }
+}
