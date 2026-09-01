12731017f1805789d41c85716dbcf8013179196a
George <georgeeasycfg@163.com>
2026-09-01 21:47:53 +0800

    20260901 night

---
+++ _01.AppLifetimeExercise/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace _01.AppLifetimeExercise
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Debug.WriteLine("OnStartup is executed");
+        }
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+            Debug.WriteLine("OnActivated is executed");
+        }
+        protected override void OnDeactivated(EventArgs e)
+        {
+            base.OnDeactivated(e);
+            Debug.WriteLine("OnDeactivated is executed");
+        }
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            Debug.WriteLine("Onexit is executed");
+        }
+    }
+
+}
---
+++ _01.AppLifetimeExercise/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _01.AppLifetimeExercise
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.SourceInitialized += (s, e) => Debug.WriteLine("001 SouceInitialized is activated.");
+            this.Activated += (s, e) => Debug.WriteLine("002 Activated is activated");
+            this.Loaded += (s, e) => Debug.WriteLine("003 loaded is activated");
+            this.ContentRendered += (s, e) => Debug.WriteLine("004 contentrendered is activated");
+            this.Deactivated += (s, e) => Debug.WriteLine("005 deactivated is activated");
+            this.Closing += (s, e) => Debug.WriteLine("006 closing is activated");
+            this.Closed += (s, e) => Debug.WriteLine("007 closed is activated");
+        }
+
+        private void MainWindow_Closed(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
