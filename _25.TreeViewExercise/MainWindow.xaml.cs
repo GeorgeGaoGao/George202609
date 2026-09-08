64d07b7f4623ff38237dc2a1521b1a3260f544cc
George <georgeeasycfg@163.com>
2026-09-08 21:27:57 +0800

    20260908 night

---
+++ _25.TreeViewExercise/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _25.TreeViewExercise
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFolderDialog openFolderDialog = new OpenFolderDialog();
+            if (openFolderDialog.ShowDialog()==true)
+            {
+                myTextBox.Text = openFolderDialog.FolderName;
+            }
+        }
+    }
+}
