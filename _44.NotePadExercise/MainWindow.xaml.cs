404b9276cedbde67ee706d8d7abf090253ee8b62
George <georgeeasycfg@163.com>
2026-09-15 21:27:17 +0800

    20260915 night

--- _44.NotePadExercise/MainWindow.xaml.cs
+++ _44.NotePadExercise/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -23,42 +25,57 @@ namespace _44.NotePadExercise
 
         private void OpenCommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-
+            e.CanExecute = true;
         }
 
         private void OpenCommandExecuted(object sender, ExecutedRoutedEventArgs e)
         {
-
+            OpenFileDialog openFileDialog = new OpenFileDialog()
+            {
+                Filter = "文本文件（*.txt）|*.txt",
+                Multiselect = false
+            };
+            if (openFileDialog.ShowDialog()==true)
+            {
+                myTextBox.Text= File.ReadAllText(openFileDialog.FileName);
+            }
         }
 
         private void CutCommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-
+            e.CanExecute = true;
         }
 
         private void CutCommandExecuted(object sender, ExecutedRoutedEventArgs e)
         {
-
+            myTextBox.Cut();
         }
 
         private void PasteCommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-
+            e.CanExecute = true;
         }
 
         private void PasteCommandExecuted(object sender, ExecutedRoutedEventArgs e)
         {
-
+            myTextBox.Paste();
         }
 
         private void SaveCommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-
+            e.CanExecute= true;
         }
 
         private void SaveCommandExecuted(object sender, ExecutedRoutedEventArgs e)
         {
-
+            SaveFileDialog saveFileDialog = new SaveFileDialog()
+            {
+                Filter="文本文件（*.txt）|*.txt",
+            };
+            if (saveFileDialog.ShowDialog()==true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, myTextBox.Text);
+            }
         }
     }
 }
