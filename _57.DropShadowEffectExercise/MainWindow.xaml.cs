118cf13ecb4ef94a297740dd0b3025be37d010c8
George <georgeeasycfg@163.com>
2026-09-20 21:27:11 +0800

    20260920 night

---
+++ _57.DropShadowEffectExercise/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _57.DropShadowEffectExercise
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point centerPoint=new Point(myGrid.ActualWidth/2, myGrid.ActualHeight/2);
+            Point mousePoint = e.GetPosition(myGrid);
+
+            double angle=Math.Atan2(mousePoint.Y-centerPoint.Y, mousePoint.X-centerPoint.X);
+            double direction = angle * 180 / Math.PI;
+
+            DropShadowEffect dropShadowEffect=myButton.Effect as DropShadowEffect;
+            dropShadowEffect.Direction=-direction;
+        }
+    }
+}
